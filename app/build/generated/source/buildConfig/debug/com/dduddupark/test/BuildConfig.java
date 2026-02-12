bbf48ba8ca8d0fcbeb03280077b61312848d4e1b
Blue <bluepark@yanadoocorp.com>
2026-02-12 14:42:37 +0900

    fix: resolve build flavor collision and add missing gradle.properties

---
+++ app/build/generated/source/buildConfig/debug/com/dduddupark/test/BuildConfig.java
@@ -0,0 +1,14 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.dduddupark.test;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "com.dduddupark.test";
+  public static final String BUILD_TYPE = "debug";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0";
+  // Field from build type: debug
+  public static final String AD_UNIT_ID_INTERSTITIAL = "ca-app-pub-3940256099942544/1033173712";
+}
